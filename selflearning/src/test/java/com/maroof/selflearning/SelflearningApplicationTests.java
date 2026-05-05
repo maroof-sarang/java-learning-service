7ed94e91ea857ade0a7b75dd50847d5f83def739
maroofsarang <maroof.sarang@gmail.com>
2026-05-05 23:31:21 +0530

    Initial Spring Boot project setup

---
+++ selflearning/src/test/java/com/maroof/selflearning/SelflearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maroof.selflearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelflearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
